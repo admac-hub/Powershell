6c3e7126239ae9335fc1f6d98a0d707e66d0d87a
Eng_A_Cuko <85145285+admac-hub@users.noreply.github.com>
2022-10-12 14:58:07 -0400

    Add files via upload

---
+++ silent unistaller.ps1
@@ -0,0 +1,52 @@
+﻿#############################################################
+#            Run unistaller in silent mode                  #
+#############################################################
+
+
+
+
+
+$path = "\\server\softwares\"
+$softwares = import-csv "\\server\softwares\pkgs.csv" -Delimiter "," -Header 'Installer','Switch' | Select-Object Installer,Switch
+
+foreach($software in $softwares){
+ 
+    $softexec = $software.Installer
+    $softexec = $softexec.ToString()
+
+    $pkgs = Get-ChildItem $path$softexec | Where-Object {$_.Name -eq $softexec}
+
+
+    foreach($pkg in $pkgs){
+   
+        $ext = [System.IO.Path]::GetExtension($pkg)
+        $ext = $ext.ToLower()
+
+        $switch = $software.Switch
+        $switch = $switch.Tostring()
+
+        if($ext -eq ".msi"){
+        mkdir c:\Temp\Softwares -Force
+        Copy-Item "$path$softexec" -Recurse c:\Temp\Softwares -Force
+        Write-host "Installing $softexec silently, please wait...." -foregroundColor Yellow
+        Start-Process "c:\Temp\Softwares\$softexec" -ArgumentList "$switch" -wait
+
+        Remove-item "c:\temp\softwares\$softexec" -Recurse -Force
+        Write-host "Installation of $softexec completed" -foregroundColor Green
+       
+        }
+        else{
+       
+        mkdir c:\Temp\Softwares -Force
+        Copy-Item "$path$softexec" -Recurse c:\Temp\Softwares -Force
+        Write-host "Installing $softexec silently, please wait...." -foregroundColor Yellow
+        Start-Process "c:\Temp\Softwares\$softexec" -ArgumentList "$switch" -wait -NoNewWindow
+
+        Remove-item "c:\temp\softwares\$softexec" -Recurse -Force
+        Write-host "Installation of $softexec completed" -foregroundColor Green
+       
+        }     
+   
+   
+    }
+}
